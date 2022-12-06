e5530d47e440165517309769966a74935c7eda23
BatuhanKaratas <karatasbatu8@gmail.com>
2022-12-07 01:22:36 +0300

    Update date.h

--- HW/Date/date.h
+++ HW/Date/date.h
@@ -26,7 +26,7 @@ namespace project {
         static constexpr int year_base = 1900;  //1
         static constexpr int random_min_year = 1940;  //2
         static constexpr int random_max_year = 2020;  //3
-        enum class Weekday { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }; //4
+        enum class Weekday : unsigned int { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }; //4
 
         Date();
         Date(int d, int m, int y);  //6
@@ -52,32 +52,41 @@ namespace project {
         Date& operator--(); //23
         Date operator--(int); //24
 
-        friend bool operator<(const Date&, const Date&); //27
-        friend bool operator==(const Date&, const Date&); //27
-
         static Date random_date(); //25
         static constexpr bool isleap(int y); //26
+        [[nodiscard]]auto convertToDayNumber() const -> int;
 
+        friend bool operator<(const Date&, const Date&); //27
+        friend bool operator==(const Date&, const Date&); //27
         friend std::ostream& operator<<(std::ostream&, const Date& date); //31
         friend std::istream& operator>>(std::istream&, Date& date); //32
 
     private:
-        void setWDayAndYearDay();
         void checkDateComponentsValueRange(const int & mday, const int & month, const int & year) const;
+        void setWDayAndYearDay();
 
-        int m_mon{};
         int m_mon_day{};
+        int m_mon{};
         int m_year{};
 
         int m_year_day{};
         int m_week_day{};
 
         static constexpr uint8_t size_of_date_array{3};
+        static const inline std::string WeekdayStr[7]{"Pazar", "Pazartesi", "Sali",
+                                                      "Carsamba", "Persembe", "Cuma",
+                                                      "Cumartesi"};
+
+        static const inline std::string MonthStr[12]{"Ocak", "Subat", "Mart",
+                                                     "Nisan", "Mayıs","Haziran",
+                                                     "Temmuz", "Agustos", "Eylul",
+                                                     "Ekim", "Kasim", "Aralik"};
+
     };
 
     bool operator<(const Date&, const Date&); //27
-    bool operator<=(const Date&, const Date&); //27
     bool operator>(const Date&, const Date&); //27
+    bool operator<=(const Date&, const Date&); //27
     bool operator>=(const Date&, const Date&); //27
     bool operator==(const Date&, const Date&); //27
     bool operator!=(const Date&, const Date&); //27
@@ -88,6 +97,9 @@ namespace project {
     Date::Weekday& operator++(Date::Weekday&); //30
     Date::Weekday operator++(Date::Weekday&, int); //30
     Date::Weekday& operator--(Date::Weekday&); //30
-    Date::Weekday operator--(Date::Weekday&, int); //30}
+    Date::Weekday operator--(Date::Weekday&, int); //30
+
+    std::ostream& operator<<(std::ostream&, const Date& date); //31
+    std::istream& operator>>(std::istream&, Date& date); //32
 }
 #endif
